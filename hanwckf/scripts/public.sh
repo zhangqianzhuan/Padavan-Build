84313b0fbbfda4c220392f5e5870848144afb130
zhangqianzhuan <119038784+zhangqianzhuan@users.noreply.github.com>
2023-05-19 11:47:17 +0800

    Update public.sh

--- hanwckf/scripts/public.sh
+++ hanwckf/scripts/public.sh
@@ -7,8 +7,8 @@
 user_name="admin"                                   # 用户名
 user_password="admin"                               # 登录密码
 lan_ip="192.168.2"                                  # LAN 地址 别写后面的 .1
-wlan_2g_ssid="Padavan"                              # 2G 无线名称
-wlan_5g_ssid="Padavan_5G"                           # 5G 无线名称
+wlan_2g_ssid="cmcc"                              # 2G 无线名称
+wlan_5g_ssid="cmcc_5G"                           # 5G 无线名称
 wlan_guest_2g_ssid="Padavan_Guset"                  # 2G 访客无线名称
 wlan_guest_5g_ssid="Padavan_Guest_5G"               # 5G 访客无线名称
 wlan_2g_psk="1234567890"                            # 2G WIFI密码最少8位 空白为不设置
